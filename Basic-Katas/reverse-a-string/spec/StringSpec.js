84423bef26b649ef50867a11dc7cb3d8e8376102
Alex <alex@scotttonge.com>
2017-12-24 08:32:11 +0000

    reverse string kata added

---
+++ Basic-Katas/reverse-a-string/spec/StringSpec.js
@@ -0,0 +1,8 @@
+describe('reverseString', () => {
+
+  it('reverses a string', () => {
+    expect(reverseString('Booya')).toEqual('ayooB');
+    expect(reverseString('Young people speaking their minds')).toEqual('sdnim rieht gnikaeps elpoep gnuoY')
+  });
+
+});
