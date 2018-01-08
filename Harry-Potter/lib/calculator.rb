350ab782c2b8535133f08b3c09f91cbfdfebc7fb
Alex <alex@scotttonge.com>
2018-01-08 12:29:55 +0000

    refactor methods in Calculator class

--- Harry-Potter/lib/calculator.rb
+++ Harry-Potter/lib/calculator.rb
@@ -8,16 +8,23 @@ class Calculator
   def initialize(basket)
     @basket = basket
     @book_price = CONST_BOOK_PRICE
-    @discount_percentage = 0.95
   end
 
   def basket_items(book_ID)
     basket.add_item(book_ID)
   end
 
+  def number_of_books
+    basket.basket_items.length
+  end
+
+  def empty_basket?
+    basket.basket_items.sum.zero?
+  end
+
   def total
-    return 0 if basket.basket_items.sum.zero?
-    basket.basket_items.length * @book_price * discount_amount
+    return 0 if empty_basket?
+    number_of_books * CONST_BOOK_PRICE * discount_amount
   end
 
   private
